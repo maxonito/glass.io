d0d2362c81853545da737e0f92239523c4eebd40
maxonito <32021927+maxonito@users.noreply.github.com>
2017-12-22 16:39:34 +0300

    fgh

---
+++ vr_files/animate-css.js
@@ -0,0 +1,14 @@
+//Animate CSS + WayPoints javaScript Plugin
+//Example: $(".element").animated("zoomInUp", "zoomOutDown");
+//Author URL: http://webdesign-master.ru
+(function($) {
+		$.fn.animated = function(inEffect, outEffect) {
+				$(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
+						if (dir === "down") {
+								$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+						};
+				}, {
+						offset: "80%"
+				});
+		};
+})(jQuery);
